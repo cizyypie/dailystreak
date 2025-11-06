49b2a84739b0a24de398ba234ce817150500af16
Joan <joanitatimbinp@gmail.com>
2025-11-06 23:08:40 +0700

    day 17

--- 90/30 day/17practice.js
+++ 90/30 day/17practice.js
@@ -27,19 +27,19 @@ getUserDataCallback(1, (user) => {
   // Output: Callback Result: { id: 1, username: 'john_doe' }
 });
 
-// Test Case Promise
-getUserDataPromise(2)
-  .then((user) => {
-    console.log('Promise Result:', user);
-    // Output: Promise Result: { id: 2, username: 'jane_smith' }
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+// // Test Case Promise
+// getUserDataPromise(2)
+//   .then((user) => {
+//     console.log('Promise Result:', user);
+//     // Output: Promise Result: { id: 2, username: 'jane_smith' }
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
 
-// Test Case Async/Await
-(async () => {
-  const user = await getUserDataAsync(3);
-  console.log('Async/Await Result:', user);
-  // Output: Async/Await Result: { id: 3, username: 'alice' }
-})();
+// // Test Case Async/Await
+// (async () => {
+//   const user = await getUserDataAsync(3);
+//   console.log('Async/Await Result:', user);
+//   // Output: Async/Await Result: { id: 3, username: 'alice' }
+// })();
